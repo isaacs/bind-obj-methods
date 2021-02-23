45eedc2bacf34ac1499cd1c74c2eb8d85034c2a0
Corey Farrell <git@cfware.com>
2021-02-23 13:04:20 -0500

    Ignore get/set/frozen property descriptors, Require Node.js 10

--- test.js
+++ test.js
@@ -4,17 +4,40 @@ var obj, m
 
 function makeObj () {
   var obj = {
-    method: function () { return this.foo },
+    method () { return this.foo },
     foo: 'bar'
   }
 
   Object.defineProperty(obj, 'secretMethod', {
-    value: function () {
+    value () {
       return 'secret' + this.method()
     },
     enumerable: false,
     configurable: true,
     writable: true
+  })
+
+  Object.defineProperty(obj, 'writable', {
+    value () {
+      return 'writable' + this.method()
+    },
+    configurable: false,
+    writable: true
+  })
+
+  Object.defineProperty(obj, 'readonly', {
+    value () {
+      return 'readonly' + (this === global ? '' : 'error')
+    },
+    configurable: false,
+    writable: false
+  })
+
+  Object.defineProperty(obj, 'getter', {
+    get () {
+      return 'getter' + (this === global ? '' : 'error')
+    },
+    configurable: true
   })
 
   return obj
@@ -41,6 +64,14 @@ m = obj.method
 t.equal(m(), 'bar')
 m = obj.secretMethod
 t.equal(m(), 'secretbar')
+m = obj.writable
+t.equal(m(), 'writablebar')
+m = obj.readonly
+t.equal(m(), 'readonly')
+
+// This is a weird thing to do but here it is.
+m = Object.getOwnPropertyDescriptor(obj, 'getter').get
+t.equal(m(), 'getter')
 
 obj = makeObj()
 bindObj(obj, Object.prototype)
